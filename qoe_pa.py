22641dd4a92d2076b17f8f8470b911d4a369ecf3
sarthak-chakraborty <sarthak.chakraborty@gmail.com>
2020-05-14 09:48:47 +0530

    Bug fixes

--- qoe_pa.py
+++ qoe_pa.py
@@ -15,20 +15,13 @@ import pickle
 dataset = int(sys.argv[1])
 topic = sys.argv[2]
 fps=int(sys.argv[3])
-<<<<<<< HEAD
-offset = int(sys.argv[4])
+offset=int(sys.argv[4])
 pref_quality = sys.argv[5]
-=======
-pref_quality = sys.argv[4]
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 
 usernum=1
 ncol_tiles=8
 nrow_tiles=8
-<<<<<<< HEAD
-=======
-pred_nframe=1
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
+pred_nframe=fps
 bitrates = {'360p':1, '480p':2.5, '720p':5, '1080p':8, '1440p':16}	# [360p, 480p, 720p, 1080p, 1440p]
 
 pref_bitrate = bitrates[pref_quality]
@@ -40,7 +33,6 @@ pref_bitrate = bitrates[pref_quality]
 # view_height = 720
 # milisec = 1000.0
 
-<<<<<<< HEAD
 # ds 1
 width=3840.0
 height=1920.0
@@ -48,29 +40,13 @@ view_width = 3840.0
 view_height = 2048.0
 milisec = 1.0
 
-# # ds 2
+# ds 2
 # width=2560.0
 # height=1280.0
 # view_width = 2560.0
 # view_height = 1440.0
 # milisec = 1.0
 
-=======
-# # ds 1
-# width=3840.0
-# height=1920.0
-# view_width = 3840.0
-# view_height = 2048.0
-# milisec = 1.0
-
-# ds 2
-width=2560.0
-height=1280.0
-view_width = 2560.0
-view_height = 1440.0
-milisec = 1.0
-
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 
 def get_data(data, frame_nos, dataset, topic, usernum):
 
@@ -81,10 +57,6 @@ def get_data(data, frame_nos, dataset, topic, usernum):
 	obj_info = np.load('Obj_traj/ds{}/ds{}_topic{}.npy'.format(dataset, dataset, topic), allow_pickle=True,  encoding='latin1').item()
 	view_info = pickle.load(open('Viewport/ds{}/viewport_ds{}_topic{}_user{}'.format(dataset, dataset, topic, usernum), 'rb'), encoding='latin1')
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 	n_objects = []
 	for i in obj_info.keys():
 		try:
@@ -97,17 +69,14 @@ def get_data(data, frame_nos, dataset, topic, usernum):
 
 	for i in range(len(view_info)-1):
 		frame = int(view_info[i][0]*1.0*fps/milisec)
-<<<<<<< HEAD
 		frame += int(offset*1.0*fps/milisec)
 
-=======
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 		frame_nos.append(frame)
 		if(frame > max_frame):
 			break
 		X={}
-		X['VIEWPORT_x']=int(view_info[i][1][0]*width/view_width)
-		X['VIEWPORT_y']=int(view_info[i][1][1]*height/view_height)
+		X['VIEWPORT_x']=int(view_info[i][1][1]*width/view_width)
+		X['VIEWPORT_y']=int(view_info[i][1][0]*height/view_height)
 		for j in range(total_objects):
 			try:
 				centroid = obj_info[frame][j]
@@ -124,38 +93,23 @@ def get_data(data, frame_nos, dataset, topic, usernum):
 				X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
 
 
-		data.append((X, int(view_info[i+1][1][0]*width/view_width),int(view_info[i+1][1][1]*height/view_height)))
+		data.append((X, int(view_info[i+1][1][1]*width/view_width),int(view_info[i+1][1][0]*height/view_height)))
 	
-	return data, frame_nos, max_frame
+	return data, frame_nos, max_frame, total_objects
 
 
 
-<<<<<<< HEAD
 def pred_frames(data, model, metric_X, metric_Y, frames, tile_manhattan_error, act_tiles, pred_tiles, count):
 	x_pred, y_pred = 0,0
-	# count = 0
-=======
-def pred_frames(data, model, metric_X, metric_Y, frames, tile_manhattan_error, act_tiles, pred_tiles):
-	x_pred, y_pred = 0,0
-	count = 0
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 
 	for k in range(len(frames)):
 		[inp_k, x_act, y_act] = data[frames[k]]
 		if(k == 0):
-<<<<<<< HEAD
-			x_pred, y_pred = model.predict_one(inp_k, True, x_act, y_act)
+			x_pred, y_pred = model.predict_one(inp_k, False, None, None)
 		else:
 			inp_k['VIEWPORT_x'] = x_pred
 			inp_k['VIEWPORT_y'] = y_pred
-			x_pred, y_pred = model.predict_one(inp_k, True, None, None)	
-=======
-			x_pred, y_pred = model.predict_one(inp_k)
-		else:
-			inp_k['VIEWPORT_x'] = x_pred
-			inp_k['VIEWPORT_y'] = y_pred
-			x_pred, y_pred = model.predict_one(inp_k)	
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
+			x_pred, y_pred = model.predict_one(inp_k, False, None, None)	
 
 		metric_X = metric_X.update(x_act, x_pred)
 		metric_Y = metric_Y.update(y_act, y_pred)
@@ -168,20 +122,6 @@ def pred_frames(data, model, metric_X, metric_Y, frames, tile_manhattan_error, a
 		actual_tile_row = nrow_tiles-1 if(actual_tile_row >= nrow_tiles) else actual_tile_row
 		actual_tile_col = ncol_tiles-1 if(actual_tile_col >= ncol_tiles) else actual_tile_col
 
-
-<<<<<<< HEAD
-		#######################################################
-=======
-		####################################################
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
-		print("x: "+str(x_act))
-		print("x_pred: "+str(x_pred))
-		print("y: "+str(y_act))	
-		print("y_pred: "+str(y_pred))
-		print("("+str(actual_tile_row)+","+str(actual_tile_col)+"),("+str(pred_tile_row)+","+str(pred_tile_col)+")")
-		#######################################################
-
-
 		act_tiles.append((actual_tile_row, actual_tile_col))
 		pred_tiles.append((pred_tile_row, pred_tile_col))
 
@@ -193,22 +133,14 @@ def pred_frames(data, model, metric_X, metric_Y, frames, tile_manhattan_error, a
 	return metric_X, metric_Y, tile_manhattan_error, count, act_tiles, pred_tiles
 
 
-def build_model(data, frame_nos, max_frame):
-<<<<<<< HEAD
-	model = linear_model.PARegressor(C=0.01, mode=2, eps=0.001, data=data, learning_rate=0.005, rho=0.99)
-=======
-	model = linear_model.PARegressor(C=0.01, mode=2, eps=0.1, data=data)
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
+def build_model(data, frame_nos, max_frame, tot_objects):
+	model = linear_model.PARegressor(C=0.01, mode=2, eps=0.1, data=data, learning_rate=0.0001, rho=0.99)
 	metric_X = metrics.MAE()
 	metric_Y = metrics.MAE()
 	manhattan_error = []
 	x_mae = []
 	y_mae = []
-<<<<<<< HEAD
-	count = 0
-	pred_nframe=30
-=======
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
+	count=0
 
 	i=0
 	tile_manhattan_error=0
@@ -225,10 +157,6 @@ def build_model(data, frame_nos, max_frame):
 		else:
 			break
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 	# Predicting frames and update model
 	while True:
 		curr_frame = frame_nos[i]
@@ -252,21 +180,16 @@ def build_model(data, frame_nos, max_frame):
 		frames = sorted(frames)
 		chunk_frames.append(frames)
 
-<<<<<<< HEAD
 		metric_X, metric_Y, tile_manhattan_error, count, act_tiles, pred_tiles = pred_frames(data, model, metric_X, metric_Y, frames, tile_manhattan_error, act_tiles, pred_tiles, count)
-=======
-		print(len(frames))
-		metric_X, metric_Y, tile_manhattan_error, count, act_tiles, pred_tiles = pred_frames(data, model, metric_X, metric_Y, frames, tile_manhattan_error, act_tiles, pred_tiles)
->>>>>>> c9f2eb3adae31f7f5f8370897d120e5b1df1e009
 		model = model.fit_n(frames)
 
 		manhattan_error.append(tile_manhattan_error*1.0 / count)
 		x_mae.append(metric_X.get())
 		y_mae.append(metric_Y.get())
 
-		print("Manhattan Tile Error: "+str(tile_manhattan_error*1.0 / count))
-		print(metric_X, metric_Y)
-		print("\n")
+		# print("Manhattan Tile Error: "+str(tile_manhattan_error*1.0 / count))
+		# print(metric_X, metric_Y)
+		# print("\n")
 
 	return act_tiles, pred_tiles, chunk_frames, manhattan_error, x_mae, y_mae
 
@@ -279,6 +202,7 @@ def alloc_bitrate(pred_tiles, frame_nos, chunk_frames, pref_quality):
 		chunk_pred = pred_tiles[chunk[0]-chunk_frames[0][0] : chunk[-1]-chunk_frames[0][0]]
 		chunk_bitrate = [[-1 for x in range(ncol_tiles)] for y in range(nrow_tiles)]
 		chunk_weight = [[1. for x in range(ncol_tiles)] for y in range(nrow_tiles)]
+
 
 		for tile in chunk_pred:
 			tile_0 = nrow_tiles-1 if(tile[0]>=nrow_tiles) else tile[0]
@@ -332,36 +256,70 @@ def calc_qoe(vid_bitrate, act_tiles, frame_nos, chunk_frames):
 	qoe = 0
 	prev_qoe_1 = 0
 	weight_1 = 1
-	weight_2 = 1
+	weight_2 = 0.5
+	weight_3 = 1
+
+	# PLayer viewport size
+	player_width = 400
+	player_height = 250
+	tile_width = width/ncol_tiles
+	tile_height = height/nrow_tiles
 
 	for i in range(len(chunk_frames)):
-		qoe_1, qoe_3 = 0, 0
+		qoe_1, qoe_2, qoe_3, qoe_4 = 0, 0, 0, 0
 		tile_count = 0
-		row, col = -1, -1
+		rows, cols = set(), set()
 		rate = []
 
 		chunk = chunk_frames[i]
 		chunk_bitrate = vid_bitrate[i]
-		chunk_act = act_tiles[chunk[0]-chunk_frames[0][0] : chunk[-1]-chunk_frames[0][0]+1]
-
+		chunk_act = act_tiles[chunk[0]-chunk_frames[0][0] : chunk[-1]-chunk_frames[0][0]]
 
 		for j in range(len(chunk_act)):
-			if(chunk_act[j][0]!=row or chunk_act[j][1]!=col):
+			if(chunk_act[j][0] not in rows or chunk_act[j][1] not in cols):
 				tile_count += 1
+			rows.add(chunk_act[j][0])
+			cols.add(chunk_act[j][1])
 			row, col = chunk_act[j][0], chunk_act[j][1]
-			qoe_1 += chunk_bitrate[row][col]
-			rate.append(chunk_bitrate[row][col])
 
+			# Find the number of tiles that can be accomodated from the center of the viewport
+			n_tiles_width = math.ceil((player_width/2 - tile_width/2)/tile_width)
+			n_tiles_height = math.ceil((player_height/2 - tile_height/2)/tile_height)
+			tot_tiles = (2*n_tiles_width+1)*(2*n_tiles_height+1)
 
+			local_qoe = 0
+			local_rate = []  # a new metric to get the standard deviation of bitrate within the player view
+			for x in range(2*n_tiles_height+1):
+				for y in range(2*n_tiles_width+1):
+					sub_row = row - n_tiles_height + x
+					sub_col = col - n_tiles_width + y
+
+					sub_row = nrow_tiles+row+sub_row if sub_row < 0 else sub_row
+					sub_col = ncol_tiles+col+sub_col if sub_col < 0 else sub_col
+					sub_row = sub_row-nrow_tiles if sub_row >= nrow_tiles else sub_row
+					sub_col = sub_col-ncol_tiles if sub_col >= ncol_tiles else sub_col
+
+					local_qoe += chunk_bitrate[sub_row][sub_col]
+					local_rate.append(chunk_bitrate[sub_row][sub_col])
+
+			qoe_1 += local_qoe / tot_tiles
+			if(len(local_rate)>0):
+				qoe_4 += np.std(local_rate)
+
+			rate.append(local_qoe / tot_tiles)
+
+		tile_count = 1 if tile_count==0 else tile_count
 		qoe_1 /= tile_count
+		qoe_4 /= tile_count
 
-		qoe_2 = np.std(rate)
+		if(len(rate)>0):
+			qoe_2 = np.std(rate)
 		qoe_2 /= tile_count
 
 		if(i>0):
 			qoe_3 = abs(prev_qoe_1 - qoe_1)
 
-		qoe += qoe_1 - weight_1*qoe_2 - weight_2*qoe_3
+		qoe += qoe_1 - weight_1*qoe_2 - weight_2*qoe_3 - weight_3*qoe_4
 		prev_qoe_1 = qoe_1
 
 	return qoe
@@ -370,9 +328,9 @@ def calc_qoe(vid_bitrate, act_tiles, frame_nos, chunk_frames):
 
 def main():
 	data, frame_nos = [],[]
-	data, frame_nos, max_frame = get_data(data, frame_nos, dataset, topic, usernum)
+	data, frame_nos, max_frame, tot_objects = get_data(data, frame_nos, dataset, topic, usernum)
 
-	act_tiles,pred_tiles,chunk_frames, manhattan_error, x_mae, y_mae = build_model(data, frame_nos, max_frame)
+	act_tiles,pred_tiles,chunk_frames, manhattan_error, x_mae, y_mae = build_model(data, frame_nos, max_frame, tot_objects)
 
 	i = 0
 	while True:
--- qoe_pa_ts.py
+++ qoe_pa_ts.py
@@ -19,7 +19,6 @@ import math
 import sys
 import pickle
 import random
-import warnings
 
 
 dataset = int(sys.argv[1])
@@ -28,7 +27,7 @@ fps=int(sys.argv[3])
 offset = int(sys.argv[4])
 pref_quality = sys.argv[5]
 
-usernum=1
+usernum=5
 ncol_tiles=8
 nrow_tiles=8
 bitrates = {'360p':1, '480p':2.5, '720p':5, '1080p':8, '1440p':16}	# [360p, 480p, 720p, 1080p, 1440p]
@@ -85,8 +84,8 @@ def get_data(data, frame_nos, dataset, topic, usernum):
 		if(frame > max_frame):
 			break
 		X={}
-		X['VIEWPORT_x']=int(view_info[i][1][1]*width/view_width)
-		X['VIEWPORT_y']=int(view_info[i][1][0]*height/view_height)
+		X['VIEWPORT_x']=int(view_info[i][1][0]*width/view_width)
+		X['VIEWPORT_y']=int(view_info[i][1][1]*height/view_height)
 		for j in range(total_objects):
 			try:
 				centroid = obj_info[frame][j]
@@ -103,35 +102,42 @@ def get_data(data, frame_nos, dataset, topic, usernum):
 				X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
 
 
-		data.append((X, int(view_info[i+1][1][1]*width/view_width),int(view_info[i+1][1][0]*height/view_height)))
+		data.append((X, int(view_info[i+1][1][0]*width/view_width),int(view_info[i+1][1][1]*height/view_height)))
 	
-	return data, frame_nos, max_frame, total_objects
+	return data, frame_nos, max_frame
 
 
 
-def pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tot_objects, tile_manhattan_error, act_tiles, pred_tiles, count):
+def pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tile_manhattan_error, act_tiles, pred_tiles, count):
+	x_pred_reg, y_pred_reg = [data[prev_frames[-1]][1]], [data[prev_frames[-1]][2]]
+	for k in range(len(frames)):
+		[inp_k, x_act, y_act] = data[frames[k]]
+		if(k == 0):
+			x_pred, y_pred = model.predict_one(inp_k, True, x_act, y_act)
+			x_pred_reg.append(x_pred)
+			y_pred_reg.append(y_pred)
+		else:
+			inp_k['VIEWPORT_x'] = x_pred
+			inp_k['VIEWPORT_y'] = y_pred
+			x_pred, y_pred = model.predict_one(inp_k, True, None, None)
+			x_pred_reg.append(x_pred)
+			y_pred_reg.append(y_pred)
+
+	x_pred_reg = np.array(x_pred_reg).reshape(len(x_pred_reg), 1)
+	y_pred_reg = np.array(y_pred_reg).reshape(len(y_pred_reg), 1)
+
+
 	series = []
-	exog = []
 	for k in range(len(prev_frames)):
 		[inp_k, x_act, y_act] = data[prev_frames[k]]
 		a = [-1 for i in range(2)]
-		e = [-1 for i in range(2 * tot_objects)]
 		for key, value in inp_k.items():
 			if key == 'VIEWPORT_x':
 				a[0] = value
 			if key == 'VIEWPORT_y':
 				a[1] = value
-			if key != 'VIEWPORT_x' and key != 'VIEWPORT_y':
-				t = key.split('_')
-				index = int(t[1])
-				if t[2] == 'x':
-					e[2*index] = value
-				else:
-					e[2*index + 1] = value
 		series.append(a)
-		exog.append(e)
 	series = np.array(series, dtype=np.float64)
-	exog = np.array(exog, dtype=np.float64)
 
 
 	result = len(series[:, 0]) > 0 and all(elem == series[0][0] for elem in series[:, 0])
@@ -143,6 +149,7 @@ def pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tot_object
 	series_log = np.log(series[:, 0])
 	series_x = np.diff(series_log, 1)
 
+
 	result = len(series[:, 1]) > 0 and all(elem == series[0][1] for elem in series[:, 1])
 	if(result):
 		series[:, 1] = [elem + random.random()/10 for elem in series[:, 1]]
@@ -152,42 +159,23 @@ def pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tot_object
 	series_log = np.log(series[:, 1])
 	series_y = np.diff(series_log, 1)
 
+
 	series_new = []
 	for i in range(len(series_x)):
 		series_new.append([series_x[i], series_y[i]])
 
 
-	# Storing exogenous variables for forecasting
-	exog_forecast = [exog[-1]]
-	for k in range(len(frames)):
-		[inp_k, x_act, y_act] = data[frames[k]]
-		e = [-1 for i in range(2 * tot_objects)]
-		for key, value in inp_k.items():
-			if key != 'VIEWPORT_x' and key != 'VIEWPORT_y':
-				t = key.split('_')
-				index = int(t[1])
-				if t[2] == 'x':
-					e[2*index] = value
-				else:
-					e[2*index + 1] = value
-		exog_forecast.append(e)
-	exog_forecast = np.array(exog_forecast, dtype=np.float64)
+	# model = VARMAX(series_new, order=(3,0))
+	# model_fit = model.fit(maxiter=1000, disp=0, method='nm', transformed=False)
+	# model_pred = model_fit.forecast(len(frames)+1)
 
+	model_x = SARIMAX(endog=series_x, order=(3,0,0), exog=series_y, seasonal_order=(1,0,0,5))
+	model_fit_x = model_x.fit(maxiter=1000, disp=0, method='nm')
+	model_pred_x = model_fit_x.forecast(len(frames)+1, exog=y_pred_reg, dynamic=True)
 
-
-	with warnings.catch_warnings():
-		warnings.filterwarnings("ignore")
-		
-		model_x = SARIMAX(series_x, order=(1,0,0))
-		model_fit_x = model_x.fit(maxiter=1000, disp=0, method='nm')
-		model_pred_x = model_fit_x.forecast(len(frames)+1)
-
-		model_y = SARIMAX(series_y, order=(3,0,0))
-		model_fit_y = model_y.fit(maxiter=1000, disp=0, method='nm')
-		model_pred_y = model_fit_y.forecast(len(frames)+1)
-
-
-	
+	model_y = SARIMAX(series_y, order=(1,0,0), exog=series_x, seasonal_order=(1,0,0,5))
+	model_fit_y = model_y.fit(maxiter=1000, disp=0, method='nm')
+	model_pred_y = model_fit_y.forecast(len(frames)+1, exog=x_pred_reg, dynamic=True)
 
 	x_pred_list, y_pred_list = [], []
 	for k in range(len(model_pred_x)):
@@ -213,8 +201,6 @@ def pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tot_object
 
 		actual_tile_row = nrow_tiles-1 if(actual_tile_row >= nrow_tiles) else actual_tile_row
 		actual_tile_col = ncol_tiles-1 if(actual_tile_col >= ncol_tiles) else actual_tile_col
-		actual_tile_row = 0 if actual_tile_row < 0 else actual_tile_row
-		actual_tile_col = 0 if actual_tile_col < 0 else actual_tile_col
 
 
 		#######################################################
@@ -237,7 +223,7 @@ def pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tot_object
 	return metric_X, metric_Y, tile_manhattan_error, count, act_tiles, pred_tiles
 
 
-def build_model(data, frame_nos, max_frame, tot_objects):
+def build_model(data, frame_nos, max_frame):
 	model = linear_model.PARegressor(C=0.01, mode=2, eps=0.001, data=data, learning_rate=0.005, rho=0.99)
 	metric_X = metrics.MAE()
 	metric_Y = metrics.MAE()
@@ -289,7 +275,7 @@ def build_model(data, frame_nos, max_frame, tot_objects):
 		frames = sorted(frames)
 		chunk_frames.append(frames)
 
-		metric_X, metric_Y, tile_manhattan_error, count, act_tiles, pred_tiles = pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tot_objects, tile_manhattan_error, act_tiles, pred_tiles, count)
+		metric_X, metric_Y, tile_manhattan_error, count, act_tiles, pred_tiles = pred_frames(data, model, metric_X, metric_Y, frames, prev_frames, tile_manhattan_error, act_tiles, pred_tiles, count)
 		model = model.fit_n(frames)
 		prev_frames = frames
 
@@ -297,9 +283,9 @@ def build_model(data, frame_nos, max_frame, tot_objects):
 		x_mae.append(metric_X.get())
 		y_mae.append(metric_Y.get())
 
-		# print("Manhattan Tile Error: "+str(tile_manhattan_error*1.0 / count))
-		# print(metric_X, metric_Y)
-		# print("\n")
+		print("Manhattan Tile Error: "+str(tile_manhattan_error*1.0 / count))
+		print(metric_X, metric_Y)
+		print("\n")
 
 	return act_tiles, pred_tiles, chunk_frames, manhattan_error, x_mae, y_mae
 
@@ -365,73 +351,36 @@ def calc_qoe(vid_bitrate, act_tiles, frame_nos, chunk_frames):
 	qoe = 0
 	prev_qoe_1 = 0
 	weight_1 = 1
-	weight_2 = 0.5
-	weight_3 = 1
-
-	# PLayer viewport size
-	player_width = 400
-	player_height = 250
-	tile_width = width/ncol_tiles
-	tile_height = height/nrow_tiles
-
-	print
+	weight_2 = 1
 
 	for i in range(len(chunk_frames)):
-		qoe_1, qoe_2, qoe_3, qoe_4 = 0, 0, 0, 0
+		qoe_1, qoe_3 = 0, 0
 		tile_count = 0
-		rows, cols = set(), set()
+		row, col = -1, -1
 		rate = []
 
 		chunk = chunk_frames[i]
 		chunk_bitrate = vid_bitrate[i]
-		chunk_act = act_tiles[chunk[0]-chunk_frames[0][0] : chunk[-1]-chunk_frames[0][0]]
+		chunk_act = act_tiles[chunk[0]-chunk_frames[0][0] : chunk[-1]-chunk_frames[0][0]+1]
 
 
 		for j in range(len(chunk_act)):
-			if(chunk_act[j][0] not in rows or chunk_act[j][1] not in cols):
+			if(chunk_act[j][0]!=row or chunk_act[j][1]!=col):
 				tile_count += 1
-			rows.add(chunk_act[j][0])
-			cols.add(chunk_act[j][1])
 			row, col = chunk_act[j][0], chunk_act[j][1]
+			qoe_1 += chunk_bitrate[row][col]
+			rate.append(chunk_bitrate[row][col])
 
-			# Find the number of tiles that can be accomodated from the center of the viewport
-			n_tiles_width = math.ceil((player_width/2 - tile_width/2)/tile_width)
-			n_tiles_height = math.ceil((player_height/2 - tile_height/2)/tile_height)
-			tot_tiles = (2*n_tiles_width+1)*(2*n_tiles_height+1)
 
-			local_qoe = 0
-			local_rate = []  # a new metric to get the standard deviation of bitrate within the player view
-			for x in range(2*n_tiles_height+1):
-				for y in range(2*n_tiles_width+1):
-					sub_row = row - n_tiles_height + x
-					sub_col = col - n_tiles_width + y
-
-					sub_row = nrow_tiles+row+sub_row if sub_row < 0 else sub_row
-					sub_col = ncol_tiles+col+sub_col if sub_col < 0 else sub_col
-					sub_row = sub_row-nrow_tiles if sub_row >= nrow_tiles else sub_row
-					sub_col = sub_col-ncol_tiles if sub_col >= ncol_tiles else sub_col
-
-					local_qoe += chunk_bitrate[sub_row][sub_col]
-					local_rate.append(chunk_bitrate[sub_row][sub_col])
-
-			qoe_1 += local_qoe / tot_tiles
-			if(len(local_rate)>0):
-				qoe_4 += np.std(local_rate)
-
-			rate.append(local_qoe / tot_tiles)
-
-		tile_count = 1 if tile_count==0 else tile_count
 		qoe_1 /= tile_count
-		qoe_4 /= tile_count
 
-		if(len(rate)>0):
-			qoe_2 = np.std(rate)
+		qoe_2 = np.std(rate)
 		qoe_2 /= tile_count
 
 		if(i>0):
 			qoe_3 = abs(prev_qoe_1 - qoe_1)
 
-		qoe += qoe_1 - weight_1*qoe_2 - weight_2*qoe_3 - weight_3*qoe_4
+		qoe += qoe_1 - weight_1*qoe_2 - weight_2*qoe_3
 		prev_qoe_1 = qoe_1
 
 	return qoe
@@ -440,9 +389,9 @@ def calc_qoe(vid_bitrate, act_tiles, frame_nos, chunk_frames):
 
 def main():
 	data, frame_nos = [],[]
-	data, frame_nos, max_frame, tot_objects = get_data(data, frame_nos, dataset, topic, usernum)
+	data, frame_nos, max_frame = get_data(data, frame_nos, dataset, topic, usernum)
 
-	act_tiles,pred_tiles,chunk_frames, manhattan_error, x_mae, y_mae = build_model(data, frame_nos, max_frame, tot_objects)
+	act_tiles,pred_tiles,chunk_frames, manhattan_error, x_mae, y_mae = build_model(data, frame_nos, max_frame)
 
 	i = 0
 	while True:
